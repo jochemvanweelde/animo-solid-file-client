692875560d9044686030f82d19192450d6e82ca4
Jochem van Weelde <jochem-van-weelde@hotmail.nl>
2021-06-17 11:42:19 +0200

    Changed code to work with node 12 and created async functions

--- src/index.js
+++ src/index.js
@@ -1,37 +1,63 @@
-import { readFile, readFileSync } from 'fs';
+import { link, readFileSync } from 'fs';
 import { SolidNodeClient } from 'solid-node-client';
 const client = new SolidNodeClient();
 
-let loginformation = await client.login({
-  idp : "https://jvwer.solidcommunity.net", 
-  username : "USERNAME_HERE",
-  password : "PASSWORD_HERE",
-});
+const solid_link = "https://jvwer.solidcommunity.net";
+const filename = "trinsic-wallet-16-6-2021-1399fdf6.txt";
+const foldername = "backup_credentials";
 
-const filename = "trinsic-wallet-16-6-2021-1399fdf6.txt"
+const account_username = "USERNAME_HERE";
+const account_password = "PASSWORD_HERE";
 
-function waitForLogin() {
-  if(!loginformation.loggedIn){
-    setTimeout(waitForLogin, 50);
-    return;
-  }
-  console.log(loginformation);
+const main = async () => {
+  //Sign in to a public accesible solid pod account
+  let login_information = await login();
+
+  //Wait for finishing signing in
+  waitForLogin( login_information );
+
+  //Write a credential file to the pod
+  writeCredentialToPod();
+
+  //Read a credential file from the pod (gibberish)
+  readCredentialFromPod();
 }
 
-waitForLogin()
+const login = async () => {
+  let loginformation = await client.login({
+    idp : solid_link, 
+    username : account_username,
+    password : account_password,
+  });
+  return loginformation;
+}
 
-var filebuffer = readFileSync("saved_credentials/" + filename);
+const waitForLogin = async ( loginformation ) => {
+  if(!loginformation.loggedIn){
+    setTimeout(waitForLogin( loginformation ), 400);
+    return;
+  }
+  console.log( loginformation );
+}
 
-console.log(filebuffer)
+const writeCredentialToPod = async () => {
+  var filebuffer = readFileSync("saved_credentials/" + filename);
 
-// write a resource
-let writeResponse = await client.fetch( 'https://jvwer.solidcommunity.net/backup_credentials/' + filename, {
-  method : "PUT",
-  body : filebuffer,
-  headers : { "Content-type" : 'text/plain' }
-});
-console.log( await writeResponse);
+  console.log( filebuffer )
 
-let readResponse = await client.fetch( 'https://jvwer.solidcommunity.net/backup_credentials/' + filename );
-// display its contents
-console.log( await readResponse.text()  );
+  // write a resource
+  let writeResponse = await client.fetch( solid_link + "/" + foldername + "/" + filename, {
+    method : "PUT",
+    body : filebuffer,
+    headers : { "Content-type" : 'text/plain' }
+  });
+  console.log( await writeResponse );
+}
+
+const readCredentialFromPod = async () => {
+  let readResponse = await client.fetch( 'https://jvwer.solidcommunity.net/backup_credentials/' + filename );
+  // display its contents
+  console.log( await readResponse.text()  );
+}
+
+main();
